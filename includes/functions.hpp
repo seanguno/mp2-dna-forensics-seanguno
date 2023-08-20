c8567d35a7ddbba6602a95d648cf9b6dfd9b3f56
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-08-19 21:40:03 -0500

    Update functions.hpp

--- includes/functions.hpp
+++ includes/functions.hpp
@@ -3,4 +3,10 @@
 
 #include "utilities.hpp"
 
+std::vector<std::string> SplitToVect(std::string line);
+int LongestConsecutiveSubstring(std::string str_to_find, std::string dna_sequence);
+std::vector<int> MaxSubs(std::vector<std::string> strs, std::string dna_sequence);
+std::vector<int> PeopleCounts(std::vector<std::vector<std::string>> ppl_vector, std::vector<int> max_subs);
+std::string GetMatch(std::vector<std::vector<std::string>> ppl_vector, std::vector<int> matches_vector);
+
 #endif
